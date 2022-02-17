955b57374c9cb93005632ba5208b19de148544af
joresatte <joresatte.joresatte@yahoo.fr>
2022-02-17 10:04:41 +0100

    cambios

--- front/src/pages/home/NewPhonePage.vue
+++ front/src/pages/home/NewPhonePage.vue
@@ -1,13 +1,14 @@
 <template>
   <h2>{{welcome}} </h2>
+  <img v-bind:src="@ /assets/firstimage.png"/>
    <section class="phone-list">
     <article
       class="phone-item"
       v-for="phone in phones"
       :key="phone.id"
     >
-    <h2>{{ phone.price }}</h2>
-    <p>{{ phone.price }}</p>
+    <h2>{{ phone.nombre }}</h2>
+    <p>{{ phone.precio }}</p>
     <p>{{ phone.price }}</p><br>
     <router-link :to="{name:'phonedetails', params:{id: phone.id}}" ><button>detaills</button></router-link><br><br>
     <button class="remove" @click="del(phone)" >remove contact</button>
@@ -20,7 +21,15 @@ export default {
     data(){
       return{
         welcome:"Welcome To Your new phone page",
-        phones: {},
+        img: '../assets/firstimage.jpg',
+        phones: [
+          {id:1, nombre:"xiaomi redmi 10", precio:"230", caracteristicas:"camera 64MP"},
+          {id:3, nombre:"xiaomi popofone", precio:"230", caracteristicas:"camera 64MP"},
+          {id:4, nombre:"samsung", precio:"230", caracteristicas:"camera 64MP"},
+          {id:5, nombre:"samsung", precio:"290", caracteristicas:"camera 64MP"},
+          {id:6, nombre:"iphone", precio:"330", caracteristicas:"camera 64MP"}, 
+          {id:7, nombre:"iphone", precio:"330", caracteristicas:"camera 64MP",},    
+        ],
         phone_removed:'',
       }
     },

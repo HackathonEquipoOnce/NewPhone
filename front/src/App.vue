a9c22feeecd42891ada33635bf796f333f34c491
joresatte <joresatte.joresatte@yahoo.fr>
2022-02-17 10:49:13 +0100

    cambios

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="nav">
-    <router-link class="home" :to="{name: 'newphone'} ">new Phone</router-link>|
+    <router-link class="home" :to="{name: 'newphone'} "><div>
+    <img src="@/assets/img/Star.png" /></div> new Phone</router-link>|
     <router-link class="newphone" :to="{name: 'newphone'}">phones</router-link>|
     <router-link class="adsposting" :to="{name: 'adsposting'}">post Ads for free</router-link>
   </div>
   <router-view />
 </template>
+<script>
+export default {
+     data(){
+       return{
+         img: './Star.png',
+       }
+     } 
+}
+</script>
 <style scoped>  
-.home{
-  background: rgb(35, 207, 230);
-  padding: 5px;
-  text-align: center;
-  border-radius: 5px;
-}
-.contacts{
-  background: rgb(9, 79, 231);
-  padding: 5px;
-  text-align: center;
-  border-radius: 5px;
-}
-.newcontact{
-  background: rgb(226, 5, 160);
-  padding: 5px;
-  text-align: center;
-  border-radius: 5px;
-}
 </style>
